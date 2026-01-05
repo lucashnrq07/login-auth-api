7b118ba5f44e6b84142935fd8812fb93b2216aac
Lucas Henrique <lucashsilvaa8@gmail.com>
2026-01-05 19:14:31 -0300

    corrige injeção no CustomUserDetailsService

--- src/main/java/com/lucas/login_auth_api/services/CustomUserDetailsService.java
+++ src/main/java/com/lucas/login_auth_api/services/CustomUserDetailsService.java
@@ -2,6 +2,7 @@ package com.lucas.login_auth_api.services;
 
 import com.lucas.login_auth_api.domain.entities.User;
 import com.lucas.login_auth_api.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -9,17 +10,18 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
 
-    @Autowired
-    private UserRepository repository;
+    private final UserRepository repository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = this.repository.findByEmail(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found"));
-        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), new ArrayList<>());
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), List.of());
     }
 }
