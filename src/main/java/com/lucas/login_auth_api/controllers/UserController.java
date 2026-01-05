b75eb1b0cb46b97c2b329b72f5f83f23565ce058
Lucas Henrique <lucashsilvaa8@gmail.com>
2026-01-05 18:38:19 -0300

    documentando UserController

--- src/main/java/com/lucas/login_auth_api/controllers/UserController.java
+++ src/main/java/com/lucas/login_auth_api/controllers/UserController.java
@@ -1,5 +1,10 @@
 package com.lucas.login_auth_api.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,9 +12,20 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
+@Tag(name = "Usuário", description = "Endpoints relacionados ao usuário autenticado")
 public class UserController {
+
+    @Operation(
+            summary = "Endpoint protegido de teste",
+            description = "Retorna uma mensagem de sucesso para usuários autenticados"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Usuário autenticado com sucesso"),
+            @ApiResponse(responseCode = "401", description = "Usuário não autenticado ou token inválido")
+    })
+    @SecurityRequirement(name = "bearerAuth")
     @GetMapping
-    public ResponseEntity<String> getUser(){
+    public ResponseEntity<String> getUser() {
         return ResponseEntity.ok("sucesso!");
     }
 }
