a55b65b463339552ca5964aabbdf4a50e5e172d2
Lucas Henrique <lucashsilvaa8@gmail.com>
2026-01-05 17:57:02 -0300

    cria SecurityFilter

--- src/main/java/com/lucas/login_auth_api/repositories/UserRepository.java
+++ src/main/java/com/lucas/login_auth_api/repositories/UserRepository.java
@@ -4,5 +4,8 @@ import com.lucas.login_auth_api.domain.entities.User;
 import jakarta.persistence.Id;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Id> {
+    Optional<User> findByEmail(String email);
 }
