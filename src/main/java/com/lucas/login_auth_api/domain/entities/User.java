e656a765a5a8a69e12f8dfe2c68ae33bfb6c79cd
Lucas Henrique <lucashsilvaa8@gmail.com>
2026-01-05 16:31:34 -0300

    cria entidade User

---
+++ src/main/java/com/lucas/login_auth_api/domain/entities/User.java
@@ -0,0 +1,27 @@
+package com.lucas.login_auth_api.domain.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+}
