7b632400f75b8b88159ccd02c0529ad614f77a66
Lucas Henrique <lucashsilvaa8@gmail.com>
2026-01-05 18:31:44 -0300

    valida DTOs

--- src/main/java/com/lucas/login_auth_api/dto/LoginRequestDTO.java
+++ src/main/java/com/lucas/login_auth_api/dto/LoginRequestDTO.java
@@ -1,3 +1,13 @@
 package com.lucas.login_auth_api.dto;
 
-public record LoginRequestDTO (String email, String password){}
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record LoginRequestDTO (
+        @Email
+        @NotBlank
+        String email,
+
+        @NotBlank
+        String password
+){}
--- src/main/java/com/lucas/login_auth_api/dto/RegisterRequestDTO.java
+++ src/main/java/com/lucas/login_auth_api/dto/RegisterRequestDTO.java
@@ -1,4 +1,16 @@
 package com.lucas.login_auth_api.dto;
 
-public record RegisterRequestDTO (String name, String email, String password) {
-}
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record RegisterRequestDTO (
+        @NotBlank
+        String name,
+
+        @Email
+        @NotBlank
+        String email,
+
+        @NotBlank
+        String password
+){}
